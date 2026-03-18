e8117305031024499fc13ffee184a94872318f7d
YashiDwivedi-786 <yashidwivedi62@gmail.com>
2026-03-18 23:58:44 +0530

    Create tryon.js

---
+++ js/tryon.js
@@ -0,0 +1,23 @@
+// Try-on
+const video = document.getElementById("camera");
+
+async function startCamera() {
+    if (!video) return;
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true
+        });
+        video.srcObject = stream;
+    } catch (error) {
+        alert("Camera access denied or not supported.");
+    }
+}
+
+
+function stopCamera() {
+    if (!video) return;
+    let stream = video.srcObject;
+    let tracks = stream.getTracks();
+    tracks.forEach(track => track.stop());
+    video.srcObject = null;
+}
